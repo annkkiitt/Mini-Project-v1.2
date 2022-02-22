f2a3330c284525b66d5076b28e989d4fd752b53f
Ankit Rawat <ankit2001rawat@gmail.com>
2022-02-22 09:48:55 +0530

    Mini-Project v1.2

--- src/components/About.js
+++ src/components/About.js
@@ -9,17 +9,17 @@ export default function About() {
   return (
     <>
     <div className="accordian">
-      <h3><img src={image1}></img>About Project</h3>
+      <h3><img src={image1} alt="img"/>About Project</h3>
       <p>So my project is on <strong>Web design and development using REACT</strong>. React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page applications.</p>
     </div>
 
     <div className="accordian">
-      <h3><img src={image2}></img>About Me</h3>
+      <h3><img src={image2} alt="img"/>About Me</h3>
       <p>I'm Ankit Rawat and currently I'm doing B.Tech in Computer Science from <strong>Graphic Era Hill University</strong>.</p>
     </div>
 
     <div className="accordian">
-      <h3><img src={image3}></img>About Website</h3>
+      <h3><img src={image3} alt="img"/>About Website</h3>
       <p><strong>TextUtils</strong> is a website which can be used to analyze text and can can be used to change case of text,copy text,remove extra space etc.</p>
     </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Components.css';
 import logo from './logo.png';
 import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
-  const [mystyle, setmystyle] = useState({})
 
-  const toggleMode=()=>{
-    if(props.mode==='light'){
-      setmystyle({
-        backgroundColor: '#8946A6'
-      })
-    }
-    else{
-      setmystyle({
-        backgroundColor: 'rgb(27, 27, 27)'
-      })
-    }
-    props.toggleMode();
-  }
   return (
     <>
     <nav className="navbar">
-      <ul className="navbarList" style={mystyle}>
-        <li><Link to="/"><img src={logo} width={40}/></Link></li>
+      <ul className="navbarList">
+        <li><Link to="/"><img src={logo} width={40} alt=""/></Link></li>
         <li><Link to="/">TextUtils</Link></li>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
